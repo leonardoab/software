8aa532f14516db5e42e2d6eab43705aa1087f14a
lbezerra <lbezerra@criptowow>
2022-12-10 13:18:31 +0000

    ajustes

--- Software.Domain/RequisitosFuncionais.js
+++ Software.Domain/RequisitosFuncionais.js
@@ -1,4 +1,9 @@
-﻿
+﻿- Cadastro de Cliente
+- Cadastro de Usuario
+- Cadastro de Contrato
+- Cadastro de Fornecedor
+- Cadastro de Plano Tarifacao
+
 - Realizar Consultas
 - Realizar Operacoes
 - Historiar Operacoes
