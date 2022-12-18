51976637eb8f85596bba68cb4110ff6d66dbeee1
Leonardo Araujo Bezerra <lbezerra@ons.org.br>
2022-12-18 15:44:12 -0300

    Tipos de arquivos

--- Software.Domain/Software/Operacao.cs
+++ Software.Domain/Software/Operacao.cs
@@ -6,11 +6,27 @@ using System.Threading.Tasks;
 
 namespace Software.Domain.Software
 {
-    public class Operacao : Entity<Guid>
+    public class HistoricoOperacao : Entity<Guid> // Operacao
     {
-        public string Descricao { get; set; }
+        public TipOperacao TipOperacao { get; set; }
+        public DateTime DataOperacao { get; set; }
+        public Contrato Contrato { get; set; }
+        public PlanoTarifacao PlanoTarifacao { get; set; }
 
+        public HistoricoOperacao(Operacao operacao, DateTime dataOperacao, Contrato contrato, PlanoTarifacao planoTarifacao)
+        {
+            if (operacao != null && contrato != null && planoTarifacao != null)
+            {
+                this.Operacao = operacao;
+                this.PlanoTarifacao = planoTarifacao;
+                this.Contrato = contrato;
+            }
+            else
+            {
+                throw new ArgumentNullException("Um Historico de operacao tem que estar associado a um contrato, operacao e um plano de tarifacao");
 
-        
+            }
+        }
+
     }
 }
---
+++ Software.Domain/Software/TipoOperacao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software.Domain.Software
+{
+    public class TipOperacao : Entity<Guid> // Tipo Operacao
+    {
+        public string Descricao { get; set; }
+
+
+        
+    }
+}
